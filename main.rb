ba240f965a00eaac26577ab3acc51c53f3bb344a
William Gadd <wgadd@sasktel.net>
2022-11-14 23:17:18 +0000

    Add generate function so that the same turn object can generate a new question every round

--- main.rb
+++ main.rb
@@ -4,9 +4,4 @@ require './question.rb'
 require './turn.rb'
 require './game.rb'
 
-players = Players.new ([1, 2])
-
-players.current_player.lose_life
-players.current_player.lose_life
-
-puts players.round_end
+game = Game.new.run
